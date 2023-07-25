2574f03c0b04b5da765411f4e017c3694da3c31d
x10combo <rakelspahi03@gmail.com>
2023-07-26 00:33:00 +0200

    Blank page when opening on browser. Fixing.

--- db.php
+++ db.php
@@ -1,2 +1,35 @@
 <?php
-$db = new SQLite3('db/database.sqlite');
+
+$databaseFile = '__DIR__';
+
+try {
+    // Connect to the SQLite database using PDO
+    $db = new PDO('sqlite:' . $databaseFile);
+    
+    // Enable PDO to throw exceptions on errors
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Create the users table if it doesn't exist
+    $db->exec('CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        username VARCHAR(50) UNIQUE,
+        password VARCHAR(255)
+    )');
+
+    // Create the tasks table if it doesn't exist
+    $db->exec('CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        title VARCHAR(100),
+        description TEXT,
+        due_date DATE,
+        status VARCHAR(20) DEFAULT "pending",
+        priority VARCHAR(20) DEFAULT "medium",
+        FOREIGN KEY (user_id) REFERENCES users (id)
+    )');
+
+    // Add any additional table creation or database initialization code here if needed.
+} catch (PDOException $e) {
+    // If an exception occurs, display the error message
+    die('Database connection failed: ' . $e->getMessage());
+}
